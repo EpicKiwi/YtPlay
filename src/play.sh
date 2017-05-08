f564cda7cc23929628d8b4f4e5bf18b03dcbd1be
EpicKiwi <lelycandu76@gmail.com>
2017-05-08 14:02:04 +0200

    Corresctions

--- src/play.sh
+++ src/play.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $2 -e "corner" ]
+if [ $2 = "corner" ]
 then
-    omxplayer --win '20 20 300 300' --aspect-mode letterbox $1
+    omxplayer --win '20 20 300 200' --aspect-mode letterbox $1
 else
     omxplayer $1
 fi
