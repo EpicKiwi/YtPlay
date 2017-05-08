1a0771dd1b2ad7d50c4337cb2941f06b50ac9ebd
EpicKiwi <me@epickiwi.fr>
2017-05-08 14:27:26 +0200

    Corrections

--- src/play.sh
+++ src/play.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if [ -z $2 ] && [ $2 -e "corner" ]
+if [ -z $2 ]
 then
-    echo "lecture dans le coin"
-    omxplayer --win '20 20 500 400' --aspect-mode letterbox $1
+    if [ $2 -e "corner" ]
+    then
+        echo "lecture dans le coin"
+        omxplayer --win '20 20 500 400' --aspect-mode letterbox $1
+    else
+        echo "Option inconnue $2"
+    fi
 else
     omxplayer --blank $1
 fi
