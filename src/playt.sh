6fce368a4eaef2330964c9e8a527def1bb090fe5
EpicKiwi <me@epickiwi.fr>
2017-05-08 14:11:31 +0200

    Corrections et echo

--- src/playt.sh
+++ src/playt.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+echo "Chargement du format de la video"
+
 FORMAT=$(youtube-dl -F $1 | tail -n1 | cut -d' ' -f1)
+
+echo "Extraction de l'URL de la video au format $FORMAT"
 
 URL=$(youtube-dl -gf $FORMAT $1)
 
+echo "Lecture de la video"
+
+BASHDIR=$(dirname $0)
+
 if [ -z $2 ]
 then
-    ./play.sh $URL $2
+    ${BASHDIR}/play.sh $URL $2
 else
-    ./play.sh $URL
+    ${BASHDIR}/play.sh $URL
 fi
